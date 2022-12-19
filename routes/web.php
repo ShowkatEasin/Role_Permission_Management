a53ddc3c061947a64f73cc0b02d9dc9a2c7b2f9f
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-12-19 14:33:12 +0600

    Revised

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,38 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@redirectAdmin')->name('index');
+Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * Admin routes
+ */
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
+    Route::resource('roles', 'Backend\RolesController', ['names' => 'admin.roles']);
+    Route::resource('users', 'Backend\UsersController', ['names' => 'admin.users']);
+    Route::resource('admins', 'Backend\AdminsController', ['names' => 'admin.admins']);
+
+
+    // Login Routes
+    Route::get('/login', 'Backend\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login/submit', 'Backend\Auth\LoginController@login')->name('admin.login.submit');
+
+    // Logout Routes
+    Route::post('/logout/submit', 'Backend\Auth\LoginController@logout')->name('admin.logout.submit');
+
+    // Forget Password Routes
+    Route::get('/password/reset', 'Backend\Auth\ForgetPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset/submit', 'Backend\Auth\ForgetPasswordController@reset')->name('admin.password.update');
 });
-
-
-
-
-
-//admin routes
-
-Route::get('/admin', function () {
-    return view('backend.layouts.master');
-});
-// Route::group(['prefix' => 'admin'], function(){
-
-//     Route::get('/admin', 'Backend/DashboardController@index')->name ('admin.dashboard');  
- 
-//     });
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
